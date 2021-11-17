6de07be52a0c613f13ba0754f2f9903918ab4a2a
Alejandro Salazar <ale_salazar@outlook.com>
2021-11-17 15:39:23 -0400

    threadlocks

--- hw6/calcServer.c
+++ hw6/calcServer.c
@@ -5,7 +5,12 @@
 /* buffer size for reading lines of input from user */
 #define LINEBUF_SIZE 1024
 
-int chat_with_client(struct Calc *calc, int infd);
+struct Calc *calc;
+int listenfd;
+pthread_mutex_t lock; 
+
+int chat_with_client(void *arg);
+void _shutdown();
 
 int main(int argc, char **argv) {
 	/* TODO: implement this program */
@@ -14,14 +19,15 @@ int main(int argc, char **argv) {
 		return -1;	
 	}
 
-	int listenfd = Open_listenfd(argv[1]);
+	listenfd = Open_listenfd(argv[1]);
 	if (listenfd == -1 || listenfd == -2) {
 		fprintf(stderr, "Error: cannot open listenfd\n");
 		return -1;
 	}
 	
-	struct Calc *calc = calc_create();
-	int open_session = 1;
+	calc = calc_create();
+
+	pthread_mutex_init(&lock, NULL);
 	
 	do {
 		int conection_request = Accept(listenfd, NULL, NULL);
@@ -29,17 +35,26 @@ int main(int argc, char **argv) {
 			fprintf(stderr, "Error: cannot accept connection request\n");
 			return -1;
 		}
-		open_session = chat_with_client(calc, conection_request);
-		close(conection_request);
-	} while(open_session);
 
-	calc_destroy(calc);
-	close(listenfd);
+		pthread_t thread_id;
+
+		// printf("Antes\n");
+		pthread_create(&thread_id, NULL, (void *)chat_with_client, (void *)conection_request);
+		// printf("Despues\n");
+
+		//open_session = chat_with_client(conection_request);
+		//close(conection_request);
+	} while(TRUE);
 
 	return 0;
 }
 
-int chat_with_client(struct Calc *calc, int infd) {
+int chat_with_client(void *arg) {
+	int infd = (int) arg;
+
+	pthread_detach(pthread_self());
+
+	printf("chat with client %d\n", infd);
 	char linebuf[LINEBUF_SIZE];
 	char output[LINEBUF_SIZE];
 
@@ -53,44 +68,64 @@ int chat_with_client(struct Calc *calc, int infd) {
 		memset(linebuf, '\0', sizeof(linebuf));
 		ssize_t n = recv(infd, (void*) &linebuf, LINEBUF_SIZE, 0);
 
+		if (n == -1) {
+			return TRUE;
+		}
+
+		if (n == 0) {
+			return TRUE;
+		}
+
 		linebuf[n] = '\0';
-		//printf("'%s'\n", linebuf);
+		// printf("'%s'\n", linebuf);
 
 		char* pch = NULL;
 		pch = strtok(linebuf, "\r\n");
-
+		// printf("AQUI2\n");
 		while (pch != NULL)
 		{
 			//sprintf(output, "-- %s ---\n", pch);
 			//write(infd, output, strlen(output));
 		
 			if(strcmp(pch, "shutdown\n") == 0 || strcmp(pch, "shutdown\r\n") == 0 || strcmp(pch, "shutdown") == 0) {
-				done = 1;
+				close(infd);
+				_shutdown();
 				return FALSE;
 			}
 			
-			if (n <= 0) {
-				/* error or end of linebuf */
-				done = 1;
-			} else if (strcmp(pch, "quit\n") == 0 || strcmp(pch, "quit\r\n") == 0 || strcmp(pch, "quit") == 0) {
+			if (strcmp(pch, "quit\n") == 0 || strcmp(pch, "quit\r\n") == 0 || strcmp(pch, "quit") == 0) {
 				/* quit command */
-				done = 1;
-				break;
-			} else {
-				/* process input line */
-				int result = -3;
-
-				if (calc_eval(calc, pch, &result) == 0) {
-					sprintf(output, "%s\n", "Error");
-					write(infd, output, strlen(output));
-				} else {
-					/* output result */
-					sprintf(output, "%i\n", result);
-					write(infd, output, strlen(output));
-				}
+				close(infd);
+				return FALSE;
 			}
+
+			/* process input line */
+			int result = -3;
+			int calc_retval = 0;
+
+			pthread_mutex_lock(&lock);
+			calc_retval = calc_eval(calc, pch, &result);
+			pthread_mutex_unlock(&lock);
+
+			if (calc_retval == 0) {
+				sprintf(output, "%s\n", "Error");
+				write(infd, output, strlen(output));
+			} else {
+				/* output result */
+				sprintf(output, "%i\n", result);
+				write(infd, output, strlen(output));
+			}
+
 			pch = strtok(NULL, "\r\n");
 		}
 	}
+	// printf("AQUI\n");
 	return TRUE;
+}
+
+void _shutdown() {
+	pthread_mutex_destroy(&lock);
+	calc_destroy(calc);
+	close(listenfd);
+	exit(0);
 }
